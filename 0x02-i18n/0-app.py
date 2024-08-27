f56d01f9a547eea6c53ff528f22742e4052e8ab0
AerinLom <aerinlom1@gmail.com>
2024-08-27 11:58:50 +0000

    Task 0: Basic Flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,11 +4,12 @@ Flask app that outputs “Welcome to Holberton” & “Hello world”
 """
 from flask import Flask, render_template
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index() -> str:
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
