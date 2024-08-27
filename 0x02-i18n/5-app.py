25377b936d34a563f79b9e20445af2542b36108f
AerinLom <aerinlom1@gmail.com>
2024-08-27 16:12:36 +0000

    Task 5: Logged in as...

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -4,6 +4,13 @@ Flask app with parametrized templates using gettext and mock login
 """
 from flask import Flask, render_template, request, g
 from flask_babel import Babel, gettext as _
+from typing import Union
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 class Config:
@@ -19,32 +26,6 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config.from_object(Config)
 babel = Babel(app)
-
-
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
-
-def get_user():
-    """
-    Mock login by user ID from URL parameter
-    """
-    user_id = request.args.get('login_as')
-    if user_id:
-        return users.get(int(user_id))
-    return None
-
-
-@app.before_request
-def before_request():
-    """
-    Set user globally if logged in
-    """
-    g.user = get_user()
 
 
 @babel.localeselector
@@ -65,14 +46,27 @@ def index() -> str:
     """
     Index page with translated text and user info
     """
-    if g.user:
-        message = _(
-            "You are logged in as {username}.".format(username=g.user['name'])
-        )
+    return render_template('5-index.html')
 
+
+def get_user() -> Union[dict, None]:
+    """
+    Returns user dict if ID can be found
+    """
+    if request.args.get('login_as'):
+        user = int(request.args.get('login_as'))
+        if user in users:
+            return users.get(user)
     else:
-        message = _("You are not logged in.")
-    return render_template('5-index.html', message=message)
+        return None
+
+
+@app.before_request
+def before_request():
+    """
+    Finds user and sets as global on flask.g.user
+    """
+    g.user = get_user()
 
 
 if __name__ == '__main__':
